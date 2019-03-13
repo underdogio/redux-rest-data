66e4f9d1744a663e86eb301ac31c7a08497fe178
Mario Pabon <me@mariopabon.com>
2019-03-13 12:21:38 -0400

    Add some more tests, with snapshots

--- test/reducer.ts
+++ test/reducer.ts
@@ -24,11 +24,41 @@ const createTestReducer = (storeName = 'test') => {
 const createTestItem = (id: string): TestDataType => {
   return {
     id: id,
-    name: 'Test item'
+    name: `Test item ${id}`
   }
 }
 
 test('Initial state', t => {
   const { initialState } = createTestReducer()
   t.snapshot(initialState)
+})
+
+test('Adding an item', t => {
+  const { initialState, reducer } = createTestReducer()
+
+  const testItem1 = createTestItem('test_id_1')
+  const state1 = reducer(initialState, {
+    type: '@underdogio/redux-rest-data/add_item',
+    storeName: 'test',
+    id: testItem1.id,
+    data: testItem1,
+    meta: {
+      error: null,
+      loading: false
+    }
+  })
+  t.snapshot(state1)
+
+  const testItem2 = createTestItem('test_id_2')
+  const state2 = reducer(state1, {
+    type: '@underdogio/redux-rest-data/add_item',
+    storeName: 'test',
+    id: testItem2.id,
+    data: testItem2,
+    meta: {
+      error: null,
+      loading: true
+    }
+  })
+  t.snapshot(state2)
 })
