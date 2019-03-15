02024091f61c5924f4f865058903d2077d1c572f
Mario Pabon <me@mariopabon.com>
2019-03-15 14:33:12 -0400

    Use Pokemon api in example

--- example/index.ts
+++ example/index.ts
@@ -1,4 +1,5 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux'
+import util from 'util'
 
 import { middleware, dataStore } from '../src'
 
@@ -18,9 +19,30 @@ async function runExample() {
 
   const store = createStore(reducer, applyMiddleware(dataStoreMiddleware))
 
-  console.log('INITIAL STATE', store.getState())
+  const logState = () => {
+    console.log(
+      'STATE',
+      util.inspect(store.getState(), {
+        depth: 4
+      })
+    )
+  }
 
+  logState()
+
+  console.log('Fetching Ditto...')
   await store.dispatch(pokemonStore.fetchItem('ditto'))
+
+  console.log('Fetching Squirtle...')
+  await store.dispatch(pokemonStore.fetchItem('squirtle'))
+
+  console.log('Fetching Raichu...')
+  await store.dispatch(pokemonStore.fetchItem('raichu'))
+
+  console.log('Fetching Dragonite...')
+  await store.dispatch(pokemonStore.fetchItem('dragonite'))
+
+  logState()
 }
 
 runExample()
