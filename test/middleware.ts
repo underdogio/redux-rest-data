15b3e6589c595eefd59ae1b9da8cd55f4136628d
Mario Pabon <me@mariopabon.com>
2019-03-15 10:55:05 -0400

    Add helper method for creating axios stub

--- test/middleware.ts
+++ test/middleware.ts
@@ -5,42 +5,46 @@ import test from 'ava'
 
 import { createDataStoreMiddleware } from '../src/middleware'
 
+function createAxiosStub() {
+  const create = stub(axios, 'create')
+  const instance = stub()
+
+  create.returns((instance as unknown) as AxiosInstance)
+
+  return {
+    create,
+    instance
+  }
+}
+
 test('Configuring middleware', t => {
-  const clientStub = stub(axios, 'create')
-  const axiosSpy = stub()
-  clientStub.returns((axiosSpy as unknown) as AxiosInstance)
+  const { create } = createAxiosStub()
 
   createDataStoreMiddleware({
     baseUrl: 'http://endpoint.api'
   })
 
-  t.deepEqual(clientStub.firstCall.args[0], {
+  t.deepEqual(create.firstCall.args[0], {
     baseURL: 'http://endpoint.api'
   })
 
-  clientStub.restore()
+  create.restore()
 })
 
 test('Starting a request', async t => {
-  const clientStub = stub(axios, 'create')
-  const axiosStub = stub()
-  clientStub.returns((axiosStub as unknown) as AxiosInstance)
+  const { create, instance } = createAxiosStub()
 
   const middleware = createDataStoreMiddleware({
     baseUrl: 'http://endpoint.api'
   })
 
-  axiosStub.returns(
-    new Promise((resolve, reject) => {
-      resolve({
-        data: {},
-        headers: {},
-        status: 200,
-        statusText: 'ok',
-        config: {}
-      })
-    })
-  )
+  instance.resolves({
+    data: {},
+    headers: {},
+    status: 200,
+    statusText: 'ok',
+    config: {}
+  })
 
   const store = createStore()()
   const next = spy()
@@ -56,11 +60,13 @@ test('Starting a request', async t => {
     url: '/item/item_id'
   })
 
-  t.deepEqual(axiosStub.firstCall.args[0], {
+  t.deepEqual(instance.firstCall.args[0], {
     headers: {
       Authorization: 'Bearer token'
     },
     method: 'get',
     url: '/item/item_id'
   })
+
+  create.restore()
 })
